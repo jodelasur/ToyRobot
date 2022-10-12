a50d9081c38504765bc89bc042e04d6771996b12
jodelasur <jasur@thinkplumb.com>
2022-10-13 06:39:12 +0800

    Simplify to just Robot class

--- toy_robot/core.py
+++ toy_robot/core.py
@@ -2,16 +2,17 @@ import re
 
 
 class Robot:
-    def __init__(self, x: int, y: int, f: str):
-        self.x = x
-        self.y = y
-        self.f = f
-
-
-class Table:
-    robot = None
+    def __init__(self):
+        self.x = None
+        self.y = None
+        self.f = None
 
     def process_command(self, cmd):
         if m := re.match(r"PLACE (\d),(\d),(\w+)", cmd):
             x_str, y_str, f = m.groups()
-            self.robot = Robot(int(x_str), int(y_str), f)
+            self._place(int(x_str), int(y_str), f)
+
+    def _place(self, x: int, y: int, f: str):
+        self.x = x
+        self.y = y
+        self.f = f
--- toy_robot/tests/test_core.py
+++ toy_robot/tests/test_core.py
@@ -1,9 +1,9 @@
-from toy_robot.core import Table
+from toy_robot.core import Robot
 
 
-def test_place_command():
-    table = Table()
-    table.process_command("PLACE 0,0,NORTH")
-    assert table.robot.x == 0
-    assert table.robot.y == 0
-    assert table.robot.f == "NORTH"
+def test_place():
+    robot = Robot()
+    robot.process_command("PLACE 0,0,NORTH")
+    assert robot.x == 0
+    assert robot.y == 0
+    assert robot.f == "NORTH"
