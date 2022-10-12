b04e628d3434fb816ba5a64f9917e1a50e29828c
jodelasur <jasur@thinkplumb.com>
2022-10-13 06:46:03 +0800

    Implement move

--- toy_robot/core.py
+++ toy_robot/core.py
@@ -11,8 +11,20 @@ class Robot:
         if m := re.match(r"PLACE (\d),(\d),(\w+)", cmd):
             x_str, y_str, f = m.groups()
             self._place(int(x_str), int(y_str), f)
+        elif cmd == "MOVE":
+            self._move()
 
     def _place(self, x: int, y: int, f: str):
         self.x = x
         self.y = y
         self.f = f
+
+    def _move(self):
+        if self.f == "NORTH":
+            self.y = self.y + 1
+        elif self.f == "SOUTH":
+            self.y = self.y - 1
+        elif self.f == "EAST":
+            self.x = self.x + 1
+        elif self.f == "WEST":
+            self.x = self.x - 1
--- toy_robot/tests/test_core.py
+++ toy_robot/tests/test_core.py
@@ -7,3 +7,12 @@ def test_place():
     assert robot.x == 0
     assert robot.y == 0
     assert robot.f == "NORTH"
+
+
+def test_move():
+    robot = Robot()
+    robot.process_command("PLACE 0,0,NORTH")
+    robot.process_command("MOVE")
+    assert robot.x == 0
+    assert robot.y == 1
+    assert robot.f == "NORTH"
