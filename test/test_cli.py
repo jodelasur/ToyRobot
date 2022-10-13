3899c05725f2cf0d4b72fb82e6f845bea9f26c3b
jodelasur <jasur@thinkplumb.com>
2022-10-13 10:30:44 +0800

    Fix incorrect assert for Example A

--- test/test_cli.py
+++ test/test_cli.py
@@ -7,7 +7,7 @@ def test_example_a(monkeypatch, capsys):
     monkeypatch.setattr(fileinput, 'input', open("test/fixtures/example_a_input.txt").readlines)
     main()
     captured = capsys.readouterr()
-    assert captured.out == "0,0,WEST\n"
+    assert captured.out == "0,1,NORTH\n"
 
 
 
