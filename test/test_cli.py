f2f79eb57e620c69ba470281499216646478d48f
jodelasur <jasur@thinkplumb.com>
2022-10-13 10:35:53 +0800

    Add Example B and C to tests

--- test/test_cli.py
+++ test/test_cli.py
@@ -10,4 +10,15 @@ def test_example_a(monkeypatch, capsys):
     assert captured.out == "0,1,NORTH\n"
 
 
+def test_example_b(monkeypatch, capsys):
+    monkeypatch.setattr(fileinput, 'input', open("test/fixtures/example_b_input.txt").readlines)
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == "0,0,WEST\n"
 
+
+def test_example_c(monkeypatch, capsys):
+    monkeypatch.setattr(fileinput, 'input', open("test/fixtures/example_c_input.txt").readlines)
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == "3,3,NORTH\n"
